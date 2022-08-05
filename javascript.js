d7c2bb7392c01483c0738669fe8cac4cb96eb96b
AlexKorenko <lexa21shane@gmail.com>
2022-08-06 01:09:24 +0300

    button sketchpad

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click',updateSketch);
+function updateSketch(){
+let inputNumber = prompt('Please, type the number of grid in your Sketchpad');
+if (inputNumber>=100||inputNumber<=-100) { return updateSketch();}
+else{
 const container = document.getElementById('container');
-let row = 16;
-let column = 16;
+let row = inputNumber;
+let column = inputNumber;
 let grid = row*column;
 for (i = 0; i<(grid); i++){
   let div = document.createElement('div');
   div.innerText = i+1;
   container.appendChild(div).className = 'div_properties';
 }
+}}
+
